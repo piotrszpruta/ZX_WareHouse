bb77625d9b680260d4d51168e973a5e4708f45b4
Piotr Szpruta <piotrszpruta@yahoo.com>
2023-05-13 22:25:18 +0200

    Add method for password hashing

---
+++ Helpers/HashPassword.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography;
+
+namespace ZX_WareHouse.Helpers;
+
+internal class HashPassword
+{
+    /// <summary>
+    /// Password hashing to SHA254
+    /// </summary>
+    /// <param name="value">Not hashed password</param>
+    /// <returns>Hashed password</returns>
+    public static string SHA254Hash(string value)
+    {
+        var byteArray = SHA256.HashData(Encoding.UTF8.GetBytes(value));
+        return Convert.ToHexString(byteArray);
+    }
+}
