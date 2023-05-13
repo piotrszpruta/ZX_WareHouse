fc043e2df3ffe63e44d4518c6a6c7800bb6595c3
Piotr Szpruta <piotrszpruta@yahoo.com>
2023-05-13 22:23:59 +0200

    Add class to check user credentials

---
+++ Helpers/ConnectionHelper.cs
@@ -0,0 +1,31 @@
+﻿using LiteDB;
+using ZX_WareHouse.Models;
+
+namespace ZX_WareHouse.Helpers;
+
+internal class ConnectionHelper
+{
+    public static string dbDefaultPath = @"./Data/warehouse.db";
+    /// <summary>
+    /// Get users collection as IEnumerable and find if credentials match
+    /// </summary>
+    /// <param name="login">Provided login</param>
+    /// <param name="password">Provided hashed password</param>
+    /// <returns>Bool depending on finding proper credentials</returns>
+    public static bool CheckUserCredentials(string login, string password)
+    {
+        using LiteDatabase db = new(dbDefaultPath);
+        var usersCol = db.GetCollection<User>("users");
+        IEnumerable<User> users = usersCol.FindAll();
+
+        foreach (User user in users)
+        {
+            if (login == user.Name && password == user.Password)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
