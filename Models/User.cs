918d89d8648c073a53050ce5eb7359fb6284cd7e
Piotr Szpruta <piotrszpruta@yahoo.com>
2023-05-13 21:44:35 +0200

    Create basic User model

---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace ZX_WareHouse.Models;
+
+class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
