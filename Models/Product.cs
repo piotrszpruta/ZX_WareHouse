efca91b16a957086a8fe02194b9d646bcfa5b042
Piotr Szpruta <piotrszpruta@yahoo.com>
2023-05-13 21:44:26 +0200

    Create basic Product model

---
+++ Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace ZX_WareHouse.Models;
+
+internal class Product
+{
+    public int Id { get; set; }
+    public string SKU { get; set; }
+    public string Model { get; set; }
+    public string Provider { get; set; }
+    public double Weight { get; set; }
+    public double NettoPrice { get; set; }
+    public int Vat { get; set; }
+}
