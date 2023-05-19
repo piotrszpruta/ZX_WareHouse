132da7322b9402b7069dfbd00a6fccdeae340722
Piotr Szpruta <piotrszpruta@yahoo.com>
2023-05-19 19:45:51 +0200

    Create method to load data to DataGridView with proper columns and brutto price calculation

--- UserControls/WarehousePanel.cs
+++ UserControls/WarehousePanel.cs
@@ -6,6 +6,32 @@
         {
             InitializeComponent();
         }
+
+    private void LoadDatabaseProducts()
+    {
+        using LiteDatabase db = new(ConnectionHelper.dbDefaultPath);
+        var productCol = db.GetCollection<Product>("products");
+        IList<Product> productList = productCol.FindAll().ToList();
+
+        DataTable dt = new();
+        dt.Columns.Add("ID");
+        dt.Columns.Add("Model");
+        dt.Columns.Add("Provider");
+        dt.Columns.Add("Quantity");
+        dt.Columns.Add("Unit");
+        dt.Columns.Add("Netto price");
+        dt.Columns.Add("VAT (%)");
+        dt.Columns.Add("Brutto price");
+        foreach (Product product in productList)
+        {
+            double bruttoPrice = product.NettoPrice + (product.NettoPrice * product.Vat / 100);
+            bruttoPrice = Math.Round(bruttoPrice, 2);
+            dt.Rows.Add(product.Id, product.Model, product.Provider, product.Quantity, product.Unit, product.NettoPrice, product.Vat, bruttoPrice);
+        }
+
+        ProductGridView.DataSource = dt;
+        ProductGridView.ClearSelection();
+    }
     private void newDeliveryToolStripMenuItem_Click(object sender, EventArgs e)
     {
         WarehouseProduct warehouseProduct = new();
