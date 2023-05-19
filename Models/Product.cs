fcda1b51358c269b49fcf506f3380c7171bac50e
Piotr Szpruta <piotrszpruta@yahoo.com>
2023-05-19 19:38:21 +0200

    Add Quantity and Unit variables, remove Weight into Product model

--- Models/Product.cs
+++ Models/Product.cs
@@ -6,7 +6,8 @@ internal class Product
     public string SKU { get; set; }
     public string Model { get; set; }
     public string Provider { get; set; }
-    public double Weight { get; set; }
+    public double Quantity { get; set; }
+    public string Unit { get; set; }
     public double NettoPrice { get; set; }
     public double Vat { get; set; }
 }
