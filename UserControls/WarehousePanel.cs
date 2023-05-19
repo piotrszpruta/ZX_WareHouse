6c26c4a77ec06a8cce2fe34b3638a93ae6ff25c9
Piotr Szpruta <piotrszpruta@yahoo.com>
2023-05-19 19:44:23 +0200

    Remove all usings and remove namespace intendation

--- UserControls/WarehousePanel.cs
+++ UserControls/WarehousePanel.cs
@@ -1,15 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿namespace ZX_WareHouse.UserControls;
 
-namespace ZX_WareHouse.UserControls
-{
     public partial class WarehousePanel : UserControl
     {
         public WarehousePanel()
