e047e5bb8fd0b209ac1ac199bf5edac59227aa6d
Piotr Szpruta <piotrszpruta@yahoo.com>
2023-05-19 19:39:26 +0200

    Create history model

---
+++ Models/History.cs
@@ -0,0 +1,15 @@
+﻿namespace ZX_WareHouse.Models;
+
+public class History
+{
+    public int Id { get; set; }
+    public string Model { get; set; }
+    public string Provider { get; set; }
+    public double Quantity { get; set; }
+    public string Unit { get; set; }
+    public double NettoPrice { get; set; }
+    public double Vat { get; set; }
+    public bool IsAdded { get; set; }
+    public DateTime Modified { get; set; }
+    public string User { get; set; }
+}
