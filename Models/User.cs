9260a7787c486dfa3dfd753b7dd6eab560edc608
Piotr Szpruta <piotrszpruta@yahoo.com>
2023-05-19 19:37:37 +0200

    Add FirstName and LastName to User model

--- Models/User.cs
+++ Models/User.cs
@@ -3,6 +3,8 @@
 class User
 {
     public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
