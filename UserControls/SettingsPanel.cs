4dc925d7a0959d5c6c83b6a982a5c71a83f0459d
Piotr Szpruta <piotrszpruta@yahoo.com>
2023-05-23 20:58:33 +0200

    Split loading users data to datagrid to external method and reference it in _Load method

--- UserControls/SettingsPanel.cs
+++ UserControls/SettingsPanel.cs
@@ -9,17 +9,25 @@ public partial class SettingsPanel : UserControl
 
     private void UsersPanel_Load(object sender, EventArgs e)
     {
-        using LiteDatabase db = new(ConnectionHelper.dbDefaultPath);
+        LoadDataToList();
+    }
+
+    private void LoadDataToList()
+    {
+        using LiteDatabase db = new(connectionString: ConnectionHelper.dbDefaultPath);
+
         var usersCol = db.GetCollection<User>("users");
         IList<User> users = usersCol.FindAll().ToList();
 
         DataTable dt = new();
         dt.Columns.Add("ID");
+        dt.Columns.Add("First name");
+        dt.Columns.Add("Last name");
         dt.Columns.Add("Login");
         dt.Columns.Add("Email");
         foreach (User user in users)
         {
-            dt.Rows.Add(user.Id, user.Name, user.Email);
+            dt.Rows.Add(user.Id, user.FirstName, user.LastName, user.Name, user.Email);
         }
 
         UserGridView.DataSource = dt;
